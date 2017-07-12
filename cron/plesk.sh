093cc18812338015163b86967de0f96343dad790
Tim Weyand <tim.weyand@klambt.de>
2017-07-12 09:21:06 +0200

    Use domainname for dnsmng

--- cron/plesk.sh
+++ cron/plesk.sh
@@ -6,11 +6,17 @@ CronFile="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )/../private/data/dynd
 if [ ! -f $CronFile ]; then
   echo "CronFile does not exist!"
   touch $CronFile;
-  /opt/psa/admin/bin/dnsmng -update
+  plesk db -r -N -e 'SELECT DISTINCT(domains.name) as domain FROM domains INNER JOIN dns_recs ON domains.dns_zone_id=dns_recs.dns_zone_id WHERE dns_recs.time_stamp >= DATE_SUB(NOW(), INTERVAL 60 MINUTE);' | while read domain; do
+    echo "Updating: $domain"
+    /opt/psa/admin/bin/dnsmng -update $domain
+  done
 fi
 
 if [[ $CacheFile -nt $CronFile ]]; then
   echo "New DynDNS Entries available"
   touch $CronFile;
-  /opt/psa/admin/bin/dnsmng -update
+  plesk db -r -N -e 'SELECT DISTINCT(domains.name) as domain FROM domains INNER JOIN dns_recs ON domains.dns_zone_id=dns_recs.dns_zone_id WHERE dns_recs.time_stamp >= DATE_SUB(NOW(), INTERVAL 60 MINUTE);' | while read domain; do
+    echo "Updating: $domain"
+    /opt/psa/admin/bin/dnsmng -update $domain
+  done
 fi
